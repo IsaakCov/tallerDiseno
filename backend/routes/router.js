e0d4c1e8b2e331775361ff7c713a55dcb8923aa0
Pedro <pedroavm16@gmail.com>
2023-08-18 11:54:39 -0400

    Update

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,23 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 // Importacion de los routers de cada controlador
-<<<<<<< HEAD
 const usuariosRouter = require("./usuariosRouter");
+const productosRouter = require('./productosRouter');
 const formRouter = require("./formularioRouter");
 
 // Uso de los routers en la API
 router.use("/usuarios", usuariosRouter);
-router.use("/formulario", formRouter);
-=======
-const usuariosRouter = require('./usuariosRouter');
-const productosRouter = require('./productosRouter');
-
-
-
-
-// Uso de los routers en la API
-router.use('/usuarios', usuariosRouter);
 router.use('/productos', productosRouter);
->>>>>>> Pedro
+router.use("/formulario", formRouter);
+
+
+
+
+
+
+
 
 module.exports = router;
