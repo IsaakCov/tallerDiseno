64d242a3327e4e8b7f459f402b99cd4699131cf7
Diego-CastilloF <131938441+Diego-CastilloF@users.noreply.github.com>
2023-08-21 12:46:08 -0400

    lala

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,13 +4,16 @@ const router = express.Router();
 // Importacion de los routers de cada controlador
 const usuariosRouter = require("./usuariosRouter");
 const formRouter = require("./formularioRouter");
+const productosRouter = require('./productosRouter');
 
 // Uso de los routers en la API
 router.use("/usuarios", usuariosRouter);
 router.use("/formulario", formRouter);
-=======
-const usuariosRouter = require('./usuariosRouter');
-const productosRouter = require('./productosRouter');
+router.use('/productos', productosRouter);
+
+
+
+
 
 
 
