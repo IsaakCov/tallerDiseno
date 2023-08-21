a82c4ecaf9b49674da563f0a655c289398527d7b
Isaac <isaakcovarrubias@gmail.com>
2023-08-21 17:07:29 -0400

    toma Diego

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,7 +1,8 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const usuarios = require('../models/usuariosModel');
-const jwt = require('jsonwebtoken');
+//const jwt = require('jsonwebtoken');
 const credentials = require('../config/const');
+const generarJWT = require('../services/generatejwt')
 
 const login = async (req, res) => {
   const { Correo, Contrasena } = req.body;
@@ -11,14 +12,16 @@ const login = async (req, res) => {
     if (!usuario) {
       return res.status(404).json({ msg: 'Usuario no encontrado' });
     }
-
-    if (bcrypt.compareSync(Contrasena, usuario.Contrasena)) {
+    const validarContrasena = bcrypt.compareSync(Contrasena, usuario.Contrasena);
+    console.log(validarContrasena);
+    if (!validarContrasena) {
+      return res.status(404).json({ msg: 'Contraseña incorrecta' });
       // Contraseña válida, inicia sesión exitosamente
-      const token = jwt.sign({ usuarioId: usuario.Correo }, credentials.jwtSecret); // Utiliza la clave secreta de las variables de entorno
+      //Sign crea el token /payload para encriptar / Secuencia de caracteres para generar el token
+    } 
+    const token = await generarJWT(Correo)
+      console.log(token);
       res.status(200).json({ msg: 'Inicio de sesión exitoso', token });
-    } else {
-      res.status(401).json({ msg: 'Contraseña incorrecta' });
-    }
   } catch (error) {
     res.status(500).json({ msg: 'Error en el servidor', error: error.message });
   }
@@ -28,8 +31,6 @@ module.exports = {
   login
 };
 
-module.exports = {
-  login
-};
+
 // Generar el JWT para poder validar credenciales, enviamos el token como respuesta
 // lo recibimos en el front, capturamos el token y lo recibimos desde el front para validar.
