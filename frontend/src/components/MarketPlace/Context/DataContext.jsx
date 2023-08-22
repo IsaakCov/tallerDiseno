8bef443038e64d6d23c9babb34099cb61378032a
Pedro <pedroavm16@gmail.com>
2023-08-22 17:03:33 -0400

    Update

--- frontend/src/components/MarketPlace/Context/DataContext.jsx
+++ frontend/src/components/MarketPlace/Context/DataContext.jsx
@@ -1,26 +1,30 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types"; // Agrega esta línea
 
 export const dataContext = createContext();
 
-const DataProvider = ({children})=> {
-    const [cart, setCart] = useState([]);
+const DataProvider = ({ children }) => {
+const initialCart = JSON.parse(localStorage.getItem("cart")) || [];
+const [cart, setCart] = useState(initialCart);
 
+useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}, [cart]);
 
-    const buyProducts = (product) => {
-        const productrepeat = cart.find((item) => item.idProducto === product.idProducto);
-        if(productrepeat){
-            setCart(cart.map((item)=>(item.idProducto === product.idProducto ? {...product, Stock: productrepeat.Stock + 1} : item)));
-        }else{
-            setCart([...cart, product]);
-        }
-        };
-    return(
-        <div>
-            <dataContext.Provider value ={{ cart, setCart, buyProducts }}>{children}</dataContext.Provider>;
-        </div>
-        
-    )
+const buyProducts = (product) => {
+    const productRepeat = cart.find((item) => item.idProducto === product.idProducto);
+    if (productRepeat) {
+    setCart(cart.map((item) => (item.idProducto === product.idProducto ? { ...product, Stock: productRepeat.Stock + 1 } : item)));
+    } else {
+    setCart([...cart, product]);
+    }
+};
+
+return <dataContext.Provider value={{ cart, setCart, buyProducts }}>{children}</dataContext.Provider>;
+};
+
+DataProvider.propTypes = {
+children: PropTypes.node.isRequired, 
 };
 
 export default DataProvider;
