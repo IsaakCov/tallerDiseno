db1811743fac257f027901cb5870dffdac37ed5f
Isaac <isaakcovarrubias@gmail.com>
2023-08-23 12:32:52 -0400

    controladores y rutas de pedidos terminados

--- backend/routes/pedidosRouter.js
+++ backend/routes/pedidosRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createPedido, deletePedido, updatePedido } = require("../controllers/pedidosController");
+const { createPedido, deletePedido, updatePedido, getPedidoById, getAllPedidos } = require("../controllers/pedidosController");
 
 
 router.post("/createPedido", createPedido);
 router.delete("/deletePedido/:id", deletePedido);
 router.put("/updatePedido/:id", updatePedido);
+router.get("/getPeidoByID/:id", getPedidoById);
+router.get("/getAllPedidos", getAllPedidos);
 module.exports = router;
