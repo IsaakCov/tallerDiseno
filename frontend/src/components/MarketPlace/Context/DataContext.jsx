0ba123897f93f0ec6eff29d67ce2731bb1a4ab4a
Diego-CastilloF <131938441+Diego-CastilloF@users.noreply.github.com>
2023-08-23 23:31:37 -0400

    localStorageCart

--- frontend/src/components/MarketPlace/Context/DataContext.jsx
+++ frontend/src/components/MarketPlace/Context/DataContext.jsx
@@ -1,26 +1,33 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 export const dataContext = createContext();
 
-const DataProvider = ({children})=> {
-    const [cart, setCart] = useState([]);
+const DataProvider = ({ children }) => {
+    // Al montar el componente, tratamos de cargar el carrito desde el localStorage
+    const savedCart = JSON.parse(localStorage.getItem('cart'));
+    const [cart, setCart] = useState(savedCart || []);
 
+    useEffect(() => {
+        // Guardamos el carrito en el localStorage cada vez que cambie
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
 
     const buyProducts = (product) => {
         const productrepeat = cart.find((item) => item.idProducto === product.idProducto);
-        if(productrepeat){
-            setCart(cart.map((item)=>(item.idProducto === product.idProducto ? {...product, Stock: productrepeat.Stock + 1} : item)));
-        }else{
+        if (productrepeat) {
+            setCart(cart.map((item) => (item.idProducto === product.idProducto ? { ...product, Stock: productrepeat.Stock + 1 } : item)));
+        } else {
             setCart([...cart, product]);
         }
-        };
-    return(
+    };
+
+    return (
         <div>
-            <dataContext.Provider value ={{ cart, setCart, buyProducts }}>{children}</dataContext.Provider>;
+            <dataContext.Provider value={{ cart, setCart, buyProducts }}>
+                {children}
+            </dataContext.Provider>
         </div>
-        
-    )
+    );
 };
 
 export default DataProvider;
