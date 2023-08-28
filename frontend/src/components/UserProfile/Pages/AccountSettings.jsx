43e931ad028a595f1f14b8a3dd0dcf70a50368e6
Fernanda Paredes <fernandaparedes0@gmail.com>
2023-08-28 14:09:10 -0400

    cambios testimonios

--- frontend/src/components/UserProfile/Pages/AccountSettings.jsx
+++ frontend/src/components/UserProfile/Pages/AccountSettings.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import "./AccountSettings.css"
+// import React from 'react';
+import "./AccountSettings.css";
 import "./YourOrders.css";
 
 const AccountSettings = () => {
@@ -9,25 +9,20 @@ const AccountSettings = () => {
       <h1 className='mainhead1'>Información Personal</h1>
       
       <div className='form'>
-      <i className="userlogo bi-person-circle"></i>
-      
-      <table className='yourOrdersTable'>
-        <thead>
-          <tr>
-            <th scope='col'>Nombre y Apellido</th>
-            <th scope='col'>Correo</th>
-            <th scope='col'>Celular/Telefono</th>
-                        
-          </tr>
-        </thead>
-      </table>
-    </div>  
+        <i className="userlogo bi-person-circle"></i>
+        
+        <table className='yourOrdersTable'>
+          <thead>
+            <tr>
+              <th scope='col'>Nombre y Apellido</th>
+              <th scope='col'>Correo</th>
+              <th scope='col'>Celular/Telefono</th>
+            </tr>
+          </thead>
+        </table>
+      </div>  
     </div>
-
-     
-    </div>
-  )
+  );
 }
 
-
-export default AccountSettings
+export default AccountSettings;
