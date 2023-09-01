7c9846cec8d7b566d4274e2bc75f226046ea0fac
Isaac <isaakcovarrubias@gmail.com>
2023-09-01 01:32:34 -0400

    deploy

--- backend/services/database.js
+++ backend/services/database.js
@@ -3,9 +3,10 @@ const {Sequelize} = require('sequelize');
 const credentials = require('../config/const');
 
 const conn = new Sequelize(credentials.dbName, credentials.dbUser, credentials.dbPass,{
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
+    host: process.env.DB_HOST,
+    dialect: process.env.DIALECT,
+    logging: false,
+    dialectOptions: process.env.SSL_OPTION
 });
 
 module.exports = conn;
